7e99b242ca18796269abdffbb5f306753bc51f43
tpm4008004-tech <tpm4008004@gmail.com>
2026-08-03 15:46:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://tpm2048_db_user:5hohMxAAEy1PQUJD@cluster0.myvazo2.mongodb.net/?appName=Cluster0';
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
